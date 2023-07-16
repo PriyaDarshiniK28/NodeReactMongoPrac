cb772d15598c5ccca575c9ed3a62bcf5580fb1c2
Priyadarshini <priyadarshinik994@gmail.com>
2023-07-16 20:16:02 +0530

    passportjs for google oauth

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 const express = require('express'); // require is common js used as node does not support ES2015 features.
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./configs/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({data: 'hey'})
-})
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+        (accessToken, refreshToken, profile, done) => {
+            console.log('accessToken', accessToken);
+            console.log('refreshToken', refreshToken);
+            console.log('profile', profile);
+        }
+    )
+)
 
-app.listen(5000);
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}
+))
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
